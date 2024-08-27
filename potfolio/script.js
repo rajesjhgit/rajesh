a4f2cc8e437c275fd1a0d0e809a0e1350bf3c059
rajesjhgit <rajesh123455667@gmail.com>
2024-08-27 17:40:56 +0545

    Portfolio

---
+++ potfolio/script.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('.navbar').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
